6afc69de248b10948f470dcc7f3a3f38ff4bd23d
Nigel Schuster <nigel.schusters@googlemail.com>
2015-12-18 17:53:59 +0100

    Inserted copyright

--- app/views/Footer.tsx
+++ app/views/Footer.tsx
@@ -9,7 +9,7 @@ interface IState {
 export default class Footer extends React.Component<IProps, IState> {
   render() {
     return (
-      <div>Footer</div>
+      <div><div className="pull-right">&copy; 2015 Nigel Schuster</div><div className="clearfix"></div></div>
     );
   }
 }
