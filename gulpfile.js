186352ae05f16a4f19b070941f901b9215d305c6
Nigel Schuster <nigel.schusters@googlemail.com>
2015-12-18 12:53:02 +0100

    Making sure to execute build steps sequentially

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ var webserver = require('gulp-webserver');
 var typescript = require('gulp-tsc');
 var clean = require('gulp-clean');
 
-gulp.task('build', function() {
+gulp.task('build', ['compile'], function() {
   return gulp.src(['tmp/App.js', "bootstrap-sass!./bootstrap-sass.config.js", "stylesheets/main.scss", "node_modules/jquery/dist/jquery.js", "node_modules/bootstrap-sass/assets/javascripts/bootstrap.js"])
     .pipe(webpack( require('./webpack.config.js') ))
     .pipe(gulp.dest('dist/'));
@@ -20,10 +20,10 @@ gulp.task('clean', ['clean'], function() {
   return gulp.src(['tmp/','dist/'], {read: false})
     .pipe(clean());
 });
-gulp.task('serve', function() {
+gulp.task('serve', ['build'], function() {
   gulp.src('dist')
     .pipe(webserver({
       livereload:true
     }));
 });
-gulp.task('default', ['compile', 'build', 'serve']);
+gulp.task('default', ['serve']);
