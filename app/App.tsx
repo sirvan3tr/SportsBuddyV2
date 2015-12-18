85bb0e61d3e768e316ca0e12519d77d8bb8ba78e
Nigel Schuster <nigel.schusters@googlemail.com>
2015-12-18 12:27:33 +0100

    Hello World - React

---
+++ app/App.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import Router from './Router';
+
+ReactDOM.render(
+  <Router />,
+  document.getElementById('container')
+);
---
+++ app/Router.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+interface IProps {
+}
+
+interface IState {
+}
+
+export default class MyRouter extends React.Component<IProps, IState> {
+  render() {
+    return <div>Hello World</div>;
+  }
+}
