2c52453673db04b75bdc5fdad4194f233cd168f3
Nigel Schuster <nigel.schusters@googlemail.com>
2015-12-19 12:22:40 +0100

    Caching authentication status

--- app/Authentication.ts
+++ app/Authentication.ts
@@ -1,9 +1,29 @@
 import $ = require('jquery');
 export default class Authentication {
+  logged : boolean;
+  logged_timeout : number;
+  logged_inTime : boolean;
+  constructor() {
+    this.logged = false;
+    this.logged_inTime = false;
+  }
+  receivedAuthStatus(status:boolean) {
+    clearTimeout(this.logged_timeout);
+    this.logged = status;
+    this.logged_timeout = setTimeout(() => {
+      this.logged_inTime = false;
+    }, 3000);
+  }
   isLoggedIn(cb:any) {
-    $.get("/isAuthenticated",function(data:any) {
-      console.log(data);
-      cb(data.auth);
-    });
+    if(this.logged_inTime == true) {
+      cb(this.logged);
+    }
+    else {
+      $.get("/isAuthenticated",(data:any) => {
+        console.log(data);
+        this.receivedAuthStatus(data.auth);
+        cb(this.logged);
+      });
+    }
   }
 }
